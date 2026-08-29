e469a75b670fbc7667c6ce91f46e7fe8bad98e1b
Rafał Kukla <loliwtf.omg@gmail.com>
2026-08-29 12:54:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='wspolny-portfel-v4-ocr';
-const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./ocr/tesseract.min.js','./ocr/worker.min.js','./ocr/tesseract-core-lstm.wasm.js','./ocr/eng.traineddata.gz'];
+const CACHE='wspolny-portfel-v5-ocr-pl';
+const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./ocr/tesseract.min.js','./ocr/worker.min.js','./ocr/tesseract-core-lstm.wasm.js','./ocr/pol.traineddata.gz'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
